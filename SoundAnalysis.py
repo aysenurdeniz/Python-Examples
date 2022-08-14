16f61e33afb51b4a45b2ec6919af3b08d65772f9
Ayşenur DENİZ <anurdenizz@gmail.com>
2022-08-14 10:20:11 +0300

    hav durumu

--- SoundAnalysis.py
+++ SoundAnalysis.py
@@ -1,6 +1,6 @@
 import time
 import speech_recognition as sr
-
+import feedparser
 
 r = sr.Recognizer()
 with sr.Microphone() as source:
@@ -11,7 +11,23 @@ with sr.Microphone() as source:
         text = r.recognize_google(data, language='tr')
         if "deneme" in text.lower():
             print("Tamam Ayşenur")
-        if "bubble" in text.lower():
+        elif "bubble" in text.lower():
             print("Efendim Ayşenur")
-        if "saat" in text.lower():
+        elif "saat" in text.lower():
             print("Saat " + str(time.localtime().tm_hour) + "'i " + str(time.localtime().tm_min) + " geçiyor.")
+        elif "hava durumu" in text.lower():
+            print("Hangi ildesin Ayşenur?")
+
+        else:
+            print("Ne demek istediğiniz anlamadım.")
+
+
+# locCode=EUR|TR|06420|ANKARA| > KITA|ULKE|POSTAKODU|IL
+def hava(il):
+    parse = feedparser.parse("http://rss.accuweather.com/rss/liveweather_rss.asp?metric=1&locCode=EUR|TR|06420|{}|" & il)
+    parse = parse["entries"][0]["summary"]
+    parse = parse.split()
+    print (parse[2], parse[4], parse[5])
+    return (hava)
+
+hava()
