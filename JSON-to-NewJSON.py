047af486de3ea5ea7e5d8d97c8a8a8ed07ae8a9d
Ayşenur DENİZ <47142377+aysenurdeniz@users.noreply.github.com>
2022-09-18 09:41:05 +0300

    Update JSON-to-NewJSON.py

--- JSON-to-NewJSON.py
+++ JSON-to-NewJSON.py
@@ -1,6 +1,7 @@
 import csv
 import json
 
+# " yerine ' ile dosyaya yazıyor
 with open("C:\\Users\\anurd\\Downloads\\googleplaystore_user_reviews.csv", "r") as f:
     reader = csv.reader(f)
     next(reader)
@@ -15,6 +16,8 @@ with open("C:\\Users\\anurd\\Downloads\\googleplaystore_user_reviews.csv", "r") 
                      "Sentiment_Subjectivity": row[4]})
         number = number + 1
 
+# \" ile " olduğu gibi kalıyor. 
+# Elasticsearch JSON formatında ' hata veriyor.
 with open("C:\\Users\\anurd\\Downloads\\googleplaystore_user_reviews.csv", "r") as f:
     reader = csv.reader(f)
     next(reader)
@@ -26,25 +29,5 @@ with open("C:\\Users\\anurd\\Downloads\\googleplaystore_user_reviews.csv", "r") 
         for row in reader:
             tx.write(st + str(number) + "\"}\n")
             tx.write(strdata[0] + str(row[0]) + strdata[1] + str(row[1]) + strdata[2] + str(row[2]) + strdata[3] + str(row[3]) + strdata[4] + str(row[4])+"\"}\n")
-            # ths.write("{'create': {'_index': 'papers', '_id': number}\n}")
-            # ths.write("{'App': {},'Translated_Review': {}, 'Sentiment': {}, 'Sentiment_Polarity': {}, "
-            #           "'Sentiment_Subjectivity': {}}\n "
-            #           .format(row[0], row[1], row[2], row[3], row[4]))
             number = number + 1
         tx.close()
-
-# with open("C:\\Users\\anurd\\PycharmProjects\\imageProcessing\\papers.json") as fil:
-#     text = fil.read()
-#     text.replace("}, {", "}/n{")
-#     fil.write(text)
-
-
-# next(veri)
-#     data = []
-#     for row in veri:
-#         line = row.replace("}, {", "}\n{")
-#         data.append(line)
-
-with open("papers.json", 'w', encoding='utf-8') as f:
-    jsonString = json.dumps(data, indent=0)
-    f.write(jsonString)
